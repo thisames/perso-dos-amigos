05f2edcc46b95ea8a5b4ea4316b46f67f383e903
Arthur Soares <arthur.mts@gmail.com>
2024-11-18 21:02:08 -0300

    create stub slash command to generate images

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import math
+
 import discord
+from imageio.v2 import imread
 import os
 import repo
+import io
+
 from team_generator.generator import generate_team
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 load_dotenv()
 bot = discord.Bot()
@@ -73,6 +80,55 @@ async def registrar(ctx, nome: str, user: discord.User):
     except Exception as e:
         print(repr(e))
 
+
+@bot.slash_command(name = "envio_imagem", description = "eita")
+async def foo(ctx):
+    images = ["https://ddragon.leagueoflegends.com/cdn/14.22.1/img/champion/Aatrox.png" for _ in range(6)]
+
+    rows = math.ceil(len(images) / 5)
+    cols = min(len(images), 5)
+
+    fig_width = cols * 2
+    fig_height = rows * 2
+
+    fig = plt.figure(figsize=(fig_width, fig_height),facecolor='gray')
+
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                     nrows_ncols=(rows, cols),  # creates 2x2 grid of axes
+                     axes_pad=0.2,  # pad between axes in inch.
+     )
+
+    for i in range(rows):
+        for j in range(cols):
+            idx = i * cols + j
+
+
+            ax = grid[idx]
+            if idx < len(images):
+                ax.text(10, 5, "Aatrox", bbox={'facecolor': 'white', 'pad': 1})
+
+                temp_image = imread(images[idx])
+                ax.imshow(temp_image)
+                ax.set_xticks([])
+                ax.set_yticks([])
+            else:
+                ax.set_facecolor('none')
+                ax.axis('off')
+
+    image_buffer = io.BytesIO()
+    fig.savefig(image_buffer, format='png')
+    image_buffer.seek(0)
+
+
+    embed = discord.Embed(
+        title = "Campeões",
+        color=discord.Color.green()
+    )
+
+    file = discord.File(fp=image_buffer, filename='image.png')
+    embed.set_image(url='attachment://image.png')
+
+    await ctx.respond(file = file, embed=embed)
 
 def main():
     bot.run(os.getenv('TOKEN'))
