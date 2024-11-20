31e09beea258c0eb2f8ec4530fd262f9223f00fb
João Paulo Dantas <joao.dantas@synchro.com.br>
2024-11-20 00:02:31 -0300

    Adding winners and victory count

--- src/main.py
+++ src/main.py
@@ -27,17 +27,30 @@ async def on_ready():
     )
 
 
-class SelectTimeView(discord.ui.View):
-    @discord.ui.select(
-        placeholder="Escolha os jogadores ativos!",
-        min_values=1,
-        max_values=repo.get_players_max_size(),
-        options=repo.get_players()
-    )
-    async def select_callback(self, select, interaction):
-        repo.add_active_players(select.values)
+class TeamSelect(discord.ui.Select):
+    async def callback(self, interaction: discord.Interaction):
+        repo.add_active_players(self.values)
 
         await interaction.message.edit("Os jogadores foram adicionados a lista de ativos.", view=None)
+
+
+class TeamSelectView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+        players = repo.get_players()
+        players_options = []
+        for player in players:
+            players_options.append(discord.SelectOption(label=player.get("nome"), value=player.id))
+
+        select = TeamSelect(
+            placeholder="Escolha os jogadores ativos!",
+            min_values=1,
+            max_values=repo.get_players_max_size(),
+            options=players_options
+        )
+
+        self.add_item(select)
 
 
 class DeleteButton(discord.ui.Button):
@@ -45,7 +58,7 @@ class DeleteButton(discord.ui.Button):
         player_id = self.custom_id
         repo.remove_active_player(player_id)
 
-        #Isso aqui tá feio demais kkkkkkkkkkkkkkkkkkkkkkkkkkk
+        # Isso aqui tá feio demais kkkkkkkkkkkkkkkkkkkkkkkkkkk
         players = repo.get_active_players()
 
         embed = discord.Embed(
@@ -55,10 +68,10 @@ class DeleteButton(discord.ui.Button):
 
         for idx, player in enumerate(players):
             player_info = repo.get_player_by_id(player)
-            embed.add_field(name=f'Jogador {idx + 1}', value=f'<@{player_info.get("discordId")}>', inline=True)
+            embed.add_field(name=f"Jogador {idx + 1}", value=f"<@{player_info.get('discord_id')}>", inline=True)
 
         await interaction.message.edit(embed=embed, view=DeleteButtons(players))
-        await interaction.response.send_message("Jogador removido!",  ephemeral=True)
+        await interaction.response.send_message("Jogador removido!", ephemeral=True)
 
 
 class DeleteButtons(discord.ui.View):
@@ -67,24 +80,41 @@ class DeleteButtons(discord.ui.View):
 
         for idx, player in enumerate(players):
             button = DeleteButton(
-                label=f'Jogador {idx + 1}',
+                label=f"Jogador {idx + 1}",
                 custom_id=player,
-                style=discord.ButtonStyle.danger
+                style=discord.ButtonStyle.red
             )
             self.add_item(button)
 
 
-@bot.slash_command(name="adicionar")
+class ResultButtons(discord.ui.View):
+    def __init__(self, match_id):
+        super().__init__(timeout=None)
+
+        self.match_id = match_id
+
+    @discord.ui.button(label="Vitoria para o time azul", style=discord.ButtonStyle.blurple)
+    async def blue_button_callback(self, button, interaction):
+        repo.set_match_victory(self.match_id, "BLUE")
+        await interaction.message.edit(view=None)
+
+    @discord.ui.button(label="Vitoria para o time vermelho", style=discord.ButtonStyle.red)
+    async def red_button_callback(self, button, interaction):
+        repo.set_match_victory(self.match_id, "RED")
+        await interaction.message.edit(view=None)
+
+
+@bot.slash_command(name="adicionar", description="Adiciona jogadores a lista de ativos")
 async def adicionar(ctx):
     if await block_trolls(ctx):
         try:
             await ctx.response.defer()
-            await ctx.followup.send("Monta o time!", view=SelectTimeView(), ephemeral=True)
+            await ctx.followup.send("Monta o time!", view=TeamSelectView(), ephemeral=True)
         except Exception as e:
             print(repr(e))
 
 
-@bot.slash_command(name="limpar")
+@bot.slash_command(name="limpar", description="Apaga todos os jogadores da lista de ativos")
 async def limpar(ctx):
     if await block_trolls(ctx):
         try:
@@ -109,7 +139,7 @@ async def ativos(ctx):
 
             for idx, player in enumerate(players):
                 player_info = repo.get_player_by_id(player)
-                embed.add_field(name=f'Jogador {idx + 1}', value=f'<@{player_info.get("discordId")}>', inline=True)
+                embed.add_field(name=f"Jogador {idx + 1}", value=f"<@{player_info.get('discord_id')}>", inline=True)
 
             await ctx.followup.send(embed=embed, view=DeleteButtons(players))
         except Exception as e:
@@ -122,24 +152,24 @@ async def sortear(ctx):
         await ctx.response.defer()
         players = repo.get_active_players()
         result = generate_team(players)
-        repo.store_match(result)
+        match_id = repo.store_match(result)
 
-        blue_embed = generate_embed(result.get('blue_team').get('champions'), discord.Colour.blue())
-        red_embed = generate_embed(result.get('red_team').get('champions'), discord.Colour.red())
+        blue_embed = generate_embed(result.get("blue_team").get("champions"), discord.Colour.blue())
+        red_embed = generate_embed(result.get("red_team").get("champions"), discord.Colour.red())
 
-        blue_team_players = ''
-        for idx, player in enumerate(result.get('blue_team').get('players')):
+        blue_team_players = ""
+        for idx, player in enumerate(result.get("blue_team").get("players")):
             player_info = repo.get_player_by_id(player)
-            player_discord = await bot.fetch_user(player_info.get("discordId"))
-            await player_discord.send(file=blue_embed['file'], embed=blue_embed['embed'])
-            blue_team_players += f'{idx+ 1} - <@{player_info.get("discordId")}>\n'
+            player_discord = await bot.fetch_user(player_info.get("discord_id"))
+            await player_discord.send(file=blue_embed["file"], embed=blue_embed["embed"])
+            blue_team_players += f"{idx + 1} - <@{player_info.get('discord_id')}>\n"
 
-        red_team_players = ''
-        for idx, player in enumerate(result.get('red_team').get('players')):
+        red_team_players = ""
+        for idx, player in enumerate(result.get("red_team").get("players")):
             player_info = repo.get_player_by_id(player)
-            player_discord = await bot.fetch_user(player_info.get("discordId"))
-            await player_discord.send(file=red_embed['file'], embed=red_embed['embed'])
-            red_team_players += f'{idx+ 1} - <@{player_info.get("discordId")}>\n'
+            player_discord = await bot.fetch_user(player_info.get("discord_id"))
+            await player_discord.send(file=red_embed["file"], embed=red_embed["embed"])
+            red_team_players += f"{idx + 1} - <@{player_info.get('discord_id')}>\n"
 
         embed = discord.Embed(
             title="Partidazuda",
@@ -148,7 +178,7 @@ async def sortear(ctx):
         )
         embed.add_field(name="Time azul (Lado esquerdo)", value=blue_team_players)
         embed.add_field(name="Time vermelho (Lado direito)", value=red_team_players)
-        await ctx.followup.send(embed=embed)
+        await ctx.followup.send(embed=embed, view=ResultButtons(match_id))
 
 
 @bot.slash_command(name="registrar", description="Adicionar jogador")
@@ -162,8 +192,47 @@ async def registrar(ctx, nome: str, user: discord.User):
             print(repr(e))
 
 
+@bot.slash_command(name="vitorias", description="Quantifica as vitorias de cada jogador")
+async def vitorias(ctx):
+    if await block_trolls(ctx):
+        await ctx.response.defer()
+        players = repo.get_players()
+        matches = repo.get_finished_matches()
+
+        player_map = {}
+        for player in players:
+            player_map[player.id] = player
+
+        victory_count = {}
+
+        for match in matches:
+            if match.get("result") == "BLUE":
+                winners = match.get("blue_team")["players"]
+            else:
+                winners = match.get("red_team")["players"]
+
+            for player in winners:
+                if player not in victory_count:
+                    victory_count[player] = 0
+
+                victory_count[player] += 1
+
+        victory_count = dict(sorted(victory_count.items(), key=lambda item: item[1], reverse=True))
+
+        result = ""
+        for idx, player in enumerate(victory_count.items()):
+            result += f"{idx+1}º <@{player_map.get(player[0]).get('discord_id')}> - {player[1]} vitorias\n"
+
+        embed = discord.Embed(
+            title="Rankzudo",
+            description=result
+        )
+
+        await ctx.followup.send(embed=embed)
+
+
 def generate_embed(champions_list, colour):
-    champion_string = ''
+    champion_string = ""
 
     rows = math.ceil(len(champions_list) / 5)
     cols = min(len(champions_list), 5)
@@ -171,12 +240,12 @@ def generate_embed(champions_list, colour):
     fig_width = cols * 2
     fig_height = rows * 2
 
-    fig = plt.figure(figsize=(fig_width, fig_height), facecolor='gray')
+    fig = plt.figure(figsize=(fig_width, fig_height), facecolor="gray")
 
     grid = ImageGrid(fig, 111,
                      nrows_ncols=(rows, cols),
                      axes_pad=0.2,
-    )
+                     )
 
     for i in range(rows):
         for j in range(cols):
@@ -184,19 +253,19 @@ def generate_embed(champions_list, colour):
 
             ax = grid[idx]
             if idx < len(champions_list):
-                champion_string += f'{champions_list[idx]["name"]}\n'
-                ax.text(10, 5, champions_list[idx]['name'], bbox={'facecolor': 'white', 'pad': 1})
+                champion_string += f"{champions_list[idx]['name']}\n"
+                ax.text(10, 5, champions_list[idx]["name"], bbox={"facecolor": "white", "pad": 1})
 
-                temp_image = imread(champions_list[idx]['image'])
+                temp_image = imread(champions_list[idx]["image"])
                 ax.imshow(temp_image)
                 ax.set_xticks([])
                 ax.set_yticks([])
             else:
-                ax.set_facecolor('none')
-                ax.axis('off')
+                ax.set_facecolor("none")
+                ax.axis("off")
 
     image_buffer = io.BytesIO()
-    fig.savefig(image_buffer, format='png')
+    fig.savefig(image_buffer, format="png")
     image_buffer.seek(0)
 
     embed = discord.Embed(
@@ -205,9 +274,9 @@ def generate_embed(champions_list, colour):
         color=colour,
     )
 
-    file = discord.File(fp=image_buffer, filename='image.png')
-    embed.set_image(url='attachment://image.png')
-    return {'embed': embed, 'file': file}
+    file = discord.File(fp=image_buffer, filename="image.png")
+    embed.set_image(url="attachment://image.png")
+    return {"embed": embed, "file": file}
 
 
 async def block_trolls(ctx):
@@ -216,7 +285,7 @@ async def block_trolls(ctx):
             title="Só a cabecinha kkkkkkkkkkkkkkkkkkkk",
         )
 
-        embed.set_image(url='https://pbs.twimg.com/media/GWQtIvhWoAA-KV0?format=jpg&name=900x900')
+        embed.set_image(url="https://pbs.twimg.com/media/GWQtIvhWoAA-KV0?format=jpg&name=900x900")
 
         await ctx.respond(embed=embed, ephemeral=True)
 
@@ -224,7 +293,7 @@ async def block_trolls(ctx):
 
 
 def main():
-    bot.run(os.getenv('TOKEN'))
+    bot.run(os.getenv("TOKEN"))
 
 
 if __name__ == "__main__":
--- src/repo.py
+++ src/repo.py
@@ -1,13 +1,13 @@
 import os
-import discord
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from google.cloud.firestore_v1.base_query import FieldFilter
 from dotenv import load_dotenv
 
 load_dotenv()
 
-cred = credentials.Certificate(os.getenv('FIREBASE_CREDENTIALS'))
+cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -15,18 +15,12 @@ db = firestore.client()
 
 def set_player(name, user):
     players_ref = db.collection("players")
-    players_ref.add({"nome": name, "discordId": user.id})
+    players_ref.add({"nome": name, "discord_id": user.id})
 
 
 def get_players():
     players_ref = db.collection("players")
-    players = players_ref.stream()
-
-    result = []
-    for player in players:
-        result.append(discord.SelectOption(label=player.get('nome'), value=str(player.id)))
-
-    return result
+    return players_ref.stream()
 
 
 def get_player_by_id(player):
@@ -41,7 +35,7 @@ def get_players_max_size():
 
 
 def add_active_players(players):
-    players_ref = db.collection("active_players").document('players')
+    players_ref = db.collection("active_players").document("players")
 
     if players_ref.get().exists:
         players_ref.update({"list": firestore.ArrayUnion(players)})
@@ -50,12 +44,12 @@ def add_active_players(players):
 
 
 def remove_active_player(player):
-    players_ref = db.collection("active_players").document('players')
+    players_ref = db.collection("active_players").document("players")
     players_ref.update({"list": firestore.ArrayRemove([player])})
 
 
 def clear_active_players():
-    players_ref = db.collection("active_players").document('players')
+    players_ref = db.collection("active_players").document("players")
     players_ref.set({"list": []})
 
 
@@ -66,6 +60,21 @@ def get_active_players():
 
 def store_match(match):
     matches_ref = db.collection("matches")
-    match['timestamp'] = firestore.SERVER_TIMESTAMP
+    match["timestamp"] = firestore.SERVER_TIMESTAMP
+    match["result"] = "UNFINISHED"
     result = matches_ref.add(match)
+    return result[1].id
 
+
+def set_match_victory(match_id, result):
+    matches_ref = db.collection("matches").document(match_id)
+    matches_ref.update({"result": result})
+
+
+def get_finished_matches():
+    matches = (
+        db.collection("matches")
+        .where(filter=FieldFilter("result", "!=", 'UNFINISHED'))
+        .stream()
+    )
+    return matches
